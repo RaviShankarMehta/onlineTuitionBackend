baf244bf40941de619070bf431d8f207753c597c
Ravi Shankar Mehta <nitravi.m@gmail.com>
2024-01-18 23:33:29 +0530

    deployemnt check

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 config({ path: "./config/config.env" });
 const app = express();
 
@@ -13,7 +15,14 @@ app.use(
     extended: true,
   })
 );
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_L0CAL_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 // Importing & Using Routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
@@ -24,7 +33,8 @@ app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
-
+app.get("/", function(req, res) {
+  res.send("Hello, this is the home page!");
+});
 app.use(ErrorMiddleware);
 export default app;
-
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  // const { connection } = await mongoose.connect(process.env.MONGO_LOCAL_URI);
-  const { connection } = await mongoose.connect(process.env.MONGO_ATLAS_URI);
+  const { connection } = await mongoose.connect(process.env.MONGO_LOCAL_URI);
+  // const { connection } = await mongoose.connect(process.env.MONGO_ATLAS_URI);
   console.log(`MongoDB connected with ${connection.host}`);
 };
