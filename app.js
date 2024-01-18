c583701975e335229c4a9b94f111e6b4789aed55
Ravi Shankar Mehta <nitravi.m@gmail.com>
2024-01-18 14:51:30 +0530

    deployemnt check

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
 config({ path: "./config/config.env" });
 const app = express();
 
 // Using Middlewares
-app.use(cors())
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -15,19 +14,16 @@ app.use(
   })
 );
 app.use(cookieParser())
-
 // Importing & Using Routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
 import payment from "./routes/paymentRoutes.js";
 import other from "./routes/otherRoutes.js";
-import home from "./routes/home.js";
 
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
-app.use("/homes", home);
 
 app.use(ErrorMiddleware);
 export default app;
